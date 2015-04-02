b03806e3e7c654f82992347eee93e8daffdf7a94
Abel Quiros <beluchin@gmail.com>
2015-04-02 06:29:36 -0400

    closes #16 - refactored the method that matches keys

--- src/main/java/recon/internal/BuildsWorkbookFromInputsImpl.java
+++ src/main/java/recon/internal/BuildsWorkbookFromInputsImpl.java
@@ -4,19 +4,19 @@ import recon.BuildsWorkbookFromInputs;
 import recon.Input;
 import recon.Workbook;
 import recon.config.Key;
-import recon.internal.datamodel.KeyMatchingOutput;
+import recon.internal.datamodel.KeyMatchingResult;
 
 import javax.annotation.Nullable;
 import javax.inject.Inject;
 
 class BuildsWorkbookFromInputsImpl implements BuildsWorkbookFromInputs {
-    private final BuildsWorkbookFromKeyMatchingOutput buildsWorkbook;
+    private final BuildsWorkbookFromKeyMatchingResult buildsWorkbook;
     private final MatchesKeysUsingSets matchesKeys;
-    private ProvidesKey providesKey;
+    private final ProvidesKey providesKey;
 
     @Inject
     BuildsWorkbookFromInputsImpl(
-            final BuildsWorkbookFromKeyMatchingOutput buildsWorkbook,
+            final BuildsWorkbookFromKeyMatchingResult buildsWorkbook,
             final MatchesKeysUsingSets matchesKeys,
             final ProvidesKey providesKey) {
         this.buildsWorkbook = buildsWorkbook;
@@ -27,9 +27,23 @@ class BuildsWorkbookFromInputsImpl implements BuildsWorkbookFromInputs {
     public @Nullable
     Workbook recon(final Input lhs, final Input rhs) {
         final Key k = providesKey.get(lhs, rhs);
-        final KeyMatchingOutput d = matchesKeys.matchKeys(lhs, rhs);
-        return d != null
+        final KeyMatchingResult d = matchesKeys.matchKeys(lhs, rhs);
+        return existsIssues(d) || existsDataColumns(lhs, rhs)
                 ? buildsWorkbook.build(k, d, lhs.getName(), rhs.getName())
                 : null;
+    }
+
+    @SuppressWarnings("UnusedParameters")
+    private boolean existsDataColumns(final Input lhs, final Input rhs) {
+        return false;
+    }
+
+    private boolean existsIssues(final KeyMatchingResult d) {
+        return existsPopulationBreaks(d);
+    }
+
+    private boolean existsPopulationBreaks(final KeyMatchingResult d) {
+        return !d.populationBreaks.getLeft().isEmpty()
+                || !d.populationBreaks.getRight().isEmpty();
     }
 }
--- src/main/java/recon/internal/BuildsWorkbookFromKeyMatchingResult.java
+++ src/main/java/recon/internal/BuildsWorkbookFromKeyMatchingResult.java
@@ -2,18 +2,18 @@ package recon.internal;
 
 import recon.Workbook;
 import recon.config.Key;
-import recon.internal.datamodel.KeyMatchingOutput;
+import recon.internal.datamodel.KeyMatchingResult;
 import recon.internal.datamodel.Worksheet;
 import recon.internal.deps.BuildsWorkbook;
 
 import javax.inject.Inject;
 
-class BuildsWorkbookFromKeyMatchingOutput {
+class BuildsWorkbookFromKeyMatchingResult {
     private final BuildsWorkbook buildsWorkbook;
     private final BuildsDataWorksheet buildsDataWorksheet;
 
     @Inject
-    BuildsWorkbookFromKeyMatchingOutput(
+    BuildsWorkbookFromKeyMatchingResult(
             final BuildsWorkbook buildsWorkbook,
             final BuildsDataWorksheet buildsDataWorksheet) {
         this.buildsWorkbook = buildsWorkbook;
@@ -22,7 +22,7 @@ class BuildsWorkbookFromKeyMatchingOutput {
 
     public Workbook build(
             final Key k,
-            final KeyMatchingOutput d,
+            final KeyMatchingResult d,
             final String lhsName,
             final String rhsName) {
         final Worksheet s = buildsDataWorksheet.build(k, d, lhsName, rhsName);
